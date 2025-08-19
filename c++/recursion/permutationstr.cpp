9c75f1072b5e79145818933b4fd79c3775afde43
Rohit10 <rsjabd17@gmail.com>
2025-08-19 22:44:20 +0530

    written code for class question

---
+++ c++/recursion/permutationstr.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+void permutation(string s,string t,int i)
+{
+    if(s == "")
+    {
+        cout<<t;
+        cout<<endl;
+        return ;
+    }
+    for(int i=0;i<s.size();i++)
+    {
+        permutation(s.substr(0,i)+s.substr(i+1,s.length()-i-1),t+s[i],i+1);
+    }
+}
+int main()
+{
+    string s;
+    cout<<"enter a string : ";
+    getline(cin,s);
+    permutation(s,"",0);
+}
