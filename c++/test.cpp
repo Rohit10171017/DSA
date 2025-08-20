447d3afaafdbc01ae86c013bfd5ca8d6afa65694
Rohit10 <rsjabd17@gmail.com>
2025-08-20 16:17:16 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,35 +1,26 @@
 #include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-using namespace std ;
-void subsetduplicate(string s,string t,bool flag)
-{
-    if(s.length()==0)
-    {
-        cout<<t<<"\n";
-        return ;
-    }
-    if(s.length() == 1)
-    {
-        if(flag == true) subsetduplicate(s.substr(1),t+s[0],true);
-        subsetduplicate(s.substr(1),t,true);
-    }
-    else if(s[0] == s[1])
-    {
-        if(flag == true) subsetduplicate(s.substr(1),t+s[0],true);
-        subsetduplicate(s.substr(1),t,false);
-    }
-    else{
-        if(flag == true) subsetduplicate(s.substr(1),t+s[0],true);
-        subsetduplicate(s.substr(1),t,true);
-    }
-}
+using namespace std;
 int main()
 {
-    string s;
-    cout<<"enetr a string : ";
-    getline(cin,s);
-    sort(s.begin(),s.end());
-    subsetduplicate(s,"",true);
+    int n;
+    cout<<"enter a size of an array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter elements of an array : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"subarrays of the given array : ";
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            for(int k=i;k<=j;k++)
+            {
+                cout<<arr[k]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
 }
