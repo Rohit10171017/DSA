530f2dba98b28f0e9156a5df16a62beb5debc9f0
Rohit10 <rsjabd17@gmail.com>
2025-08-21 22:03:44 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,26 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+void maze(int n,int m ,string t)
+{
+    if(n<1 || m<1) return ;
+    if(n==1 && m==1)
+    {
+        cout<<t<<endl;
+        return ;
+    }
+    maze(n-1,m,t+"D") ;
+    maze(n,m-1,t+"R");
+}
 int main()
 {
     int n;
-    cout<<"enter a size of an array : ";
-    cin>>n;
-    int arr[n];
-    cout<<"enter elements of an array : ";
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    cout<<"subarrays of the given array : ";
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i;j<n;j++)
-        {
-            for(int k=i;k<=j;k++)
-            {
-                cout<<arr[k]<<" ";
-            }
-            cout<<"\n";
-        }
-    }
+    cout << "enter no of rows : ";
+    cin >> n;
+    int m;
+    cout<<"enter no of columns : ";
+    cin>>m;
+    maze(n,m,"");
+    return 0;
 }
