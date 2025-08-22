7e5027e1e0a9a09ae934bedbe62903a54e030425
Rohit10 <rsjabd17@gmail.com>
2025-08-22 09:15:14 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,24 +1,13 @@
-#include <iostream>
-using namespace std;
-void maze(int n,int m ,string t)
-{
-    if(n<1 || m<1) return ;
-    if(n==1 && m==1)
-    {
-        cout<<t<<endl;
-        return ;
-    }
-    maze(n-1,m,t+"D") ;
-    maze(n,m-1,t+"R");
-}
+#include<iostream>
+#include<vector>
+using namespace std ;
 int main()
 {
     int n;
-    cout << "enter no of rows : ";
-    cin >> n;
-    int m;
-    cout<<"enter no of columns : ";
-    cin>>m;
-    maze(n,m,"");
+    cin>>n;
+    vector<int>v(n);
+    cout<<v.size()<<" "<<v.capacity()<<endl;
+    v.push_back(3);
+    cout<<v.size()<<" "<<v.capacity();
     return 0;
 }
