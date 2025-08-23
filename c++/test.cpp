e0b944e1eded4e3a723455b217879c27afeb446a
Rohit10 <rsjabd17@gmail.com>
2025-08-23 09:50:05 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
-#include<vector>
-using namespace std ;
+#include<climits>
+using namespace std;
+int print(int arr[],int n)
+{
+    if(n<0) 
+    {
+        return INT_MIN;
+    }
+    return max(arr[n],print(arr,n-1));
+
+}
 int main()
 {
     int n;
+    cout<<"enter size of an array  : ";
     cin>>n;
-    vector<int>v(n);
-    cout<<v.size()<<" "<<v.capacity()<<endl;
-    v.push_back(3);
-    cout<<v.size()<<" "<<v.capacity();
-    return 0;
+    int arr[n];
+    cout<<"enter elements of an array : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>i[arr];
+    }
+    cout<<"\n";
+    cout<<print(arr,n-1);
 }
