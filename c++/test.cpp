d474ae963e433bef67ef94b9f7902fb1c36d1695
Rohit10 <rsjabd17@gmail.com>
2025-08-26 16:35:56 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,26 +1,6 @@
 #include<iostream>
-#include<climits>
 using namespace std;
-int print(int arr[],int n)
-{
-    if(n<0) 
-    {
-        return INT_MIN;
-    }
-    return max(arr[n],print(arr,n-1));
-
-}
 int main()
 {
-    int n;
-    cout<<"enter size of an array  : ";
-    cin>>n;
-    int arr[n];
-    cout<<"enter elements of an array : ";
-    for(int i=0;i<n;i++)
-    {
-        cin>>i[arr];
-    }
-    cout<<"\n";
-    cout<<print(arr,n-1);
+    cout<<9876543210%1234567890;
 }
