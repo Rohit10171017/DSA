5f9c811bc39b776953bf8363f64de597936ba7cc
Rohit10 <rsjabd17@gmail.com>
2025-09-02 09:44:31 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
 using namespace std;
 int main()
 {
-    cout<<9876543210%1234567890;
+    string s;
+    cout<<"enter a string : ";
+    getline(cin,s);
+    vector<int>v(150);
+    for(int i=0;i<s.size();i++)
+    {
+        v[(int)s[i]]++;
+    }
+    int max = 0;;
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i]>max) max = v[i];
+    }  
+    for(int i=0;i<v.size();i++)
+    {
+        if(v[i]==max) cout<<(char)(i)<<"->"<<max<<"\n";
+    }
+    return 0;
 }
