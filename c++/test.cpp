de6f685b7eb6dcd3b6226415475ffddccecb025f
Rohit10 <rsjabd17@gmail.com>
2025-09-22 21:31:15 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,31 +1,22 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
+#include<climits>
 using namespace std;
+int maximum(int arr[],int n)
+{
+    if( n < 0 ) return INT_MIN;
+    else return max(arr[n],maximum(arr,n-1));
+}
 int main()
 {
     int n;
     cout<<"enter a size of an array : ";
     cin>>n;
-    vector<int>arr(n);
+    int arr[n];
+    cout<<"enter elements of an array : ";
     for(int &ele : arr)
     {
         cin>>ele;
     }
-    int target ;
-    cout<<"enter a target : ";
-    cin>>target;
-    int i=0,j=n-1;
-    sort(arr.begin(),arr.end());
-    while(i<j)
-    {
-        if(arr[i]>= target) break;
-        else if(arr[j]>=target) j--;
-        else if(arr[i] > (target-arr[j])) j--;
-        else if(arr[i] < (target-arr[j])) i++;
-        else if(arr[i] + arr[j] == target) 
-        {
-            cout<<"("<<arr[i++]<<" "<<arr[j--]<<")"<<endl;
-        }
-    }    
+    cout<<maximum(arr,n-1);
+    return 0;
 }
