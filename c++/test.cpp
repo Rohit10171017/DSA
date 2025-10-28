a3927bca5cc55c5d4dfe70076a2e0dd3a2eb476c
Rohit10 <rsjabd17@gmail.com>
2025-10-28 22:35:41 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
+#include<string>
 using namespace std;
-int kthsymbol(int n ,int k)
+string countandsay(int n,string t,int j)
 {
-   
+    if(n==1) return "1";
+    if(j == n ) return t;
+    int i = 0;
+    string s="";
+    int c = 1;
+    while(t[i] != '\0')
+    {
+        if(t[i]==t[i+1] && i != t.size()-1) c++;
+        else{
+            s = s + to_string(c) + t[i];
+            c = 1;
+        }
+        i++;
+    }
+    return countandsay(n,s,j+1);
 }
 int main()
 {
     int n;
-    cout<<"enter no of rows : ";
+    cout<<"enter a number : ";
     cin>>n;
-    int k ;
-    cout<<"enter position of the element : ";
-    cin>>k;
-    cout<<kthsymbol(n,k);
+    cout<<countandsay(n,"1",1);
 }
