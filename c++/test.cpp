31ffc2ef893827360b6c58d9caaeb5c6d46001a5
Rohit10 <rsjabd17@gmail.com>
2026-01-03 10:08:01 +0530

    practice

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,46 +1,45 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
-void nextperm(vector<int>&v)
-{
-  int n = v.size()-1;
-  int x = -1;
-
-for(int i = n ; i >=1 ; i--)
-{
-  if(v[i-1] < v[i]){
-    x = i - 1;
-    break;
-  }
-}
-if( x == -1) reverse(v.begin(),v.end());
-else{
-  reverse(v.begin()+x+1,v.end());
-  for(int i = x + 1 ; i <= n ; i++)
-  {
-    if(v[i] > v[x]) 
-    {
-      swap(v[i],v[x]);
-      break;
-    }
-  }
-}
-}
 int main()
 {
-  int n;
-  cout<<"Enter the size of an aray : ";
-  cin>>n;
-  vector<int>v(n);
-  cout<<"enter a elements of an array : ";
-  for(auto &ele : v)
+  string s,p,r;
+  cout<<"enter a initial string : ";
+  getline(cin,s);
+  cout<<"enter a pattern string : ";
+  getline(cin,p);
+  cout<<"Enter a replacements string : ";
+  getline(cin,r);
+  int k = 0;
+  int l = 0;
+  bool flag ;
+  string result = "";
+  for(int i = 0 ; i < s.length() ; i++)
   {
-    cin>>ele;
+    if(s[i] == p[k++])
+    {
+      l++;
+      flag = true;
+    }
+    else 
+    {
+      flag == false;
+      k = 0;
+    }
+    if(l == 0){
+      result += s[i];
+      k = 0;
+    }
+    if(l == p.length() && flag == true)
+    {
+      result += r;
+      l = 0;
+      k = 0;
+    }
+    else if(flag == false && l != 0){
+      result += s.substr(i-l,l);
+      l = 0;
+      k = 0;
+    }
   }
-  nextperm(v);
-  for(auto ele : v)
-  {
-    cout<<ele<<" ";
-  }
+  cout<<result;
 }
